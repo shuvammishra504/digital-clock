eb873452f54174c51cfbd6ea999f071edb70ad2a
Shuvam <shuvamishra1920@gmail.com>
2021-05-09 11:01:58 +0545

    bla bla bla

--- clock.js
+++ clock.js
@@ -5,7 +5,9 @@ const barsa = document.querySelector('.barsa');
 const day = document.querySelector('.day')
 
 const samayaDiv = document.querySelector('.time');
-const samaya = document.querySelector('.samaya');
+const hour = document.querySelector('.hour');
+const minute = document.querySelector('.minute');
+const second = document.querySelector('.second');
 
 function NepalDate(){
     const time = Date();
@@ -14,12 +16,23 @@ function NepalDate(){
     var tarikhValue = time.slice(8, 10);
     var din = time.slice(0, 4);
     var year = time.slice(11, 15);
-    var ghadi = time.slice(16, 21);
+    var ghanta = time.slice(16,18);
+    var minut = time.slice(19, 21);
+    var sec = time.slice(22, 24);
 
     day.innerHTML = din;
     maina.innerHTML = month;
     tarikh.innerHTML = tarikhValue;
-    samaya.innerHTML = ghadi;
+    barsa.innerHTML = year
+    hour.innerHTML = ghanta;
+    minute.innerHTML = minut;
+    second.innerHTML = sec;
 }
 
 setInterval(NepalDate, 1000);
+
+
+var time = new Date();
+console.log(
+  time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true })
+);
