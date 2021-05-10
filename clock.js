c1b6c86d55926447451c21c321642b88779c679a
Shuvam <shuvamishra1920@gmail.com>
2021-05-10 10:34:00 +0545

    functional

--- clock.js
+++ clock.js
@@ -5,34 +5,30 @@ const barsa = document.querySelector('.barsa');
 const day = document.querySelector('.day')
 
 const samayaDiv = document.querySelector('.time');
-const hour = document.querySelector('.hour');
-const minute = document.querySelector('.minute');
-const second = document.querySelector('.second');
+const samaya = document.querySelector('.samaya'); 
 
 function NepalDate(){
     const time = Date();
-    console.log(time)
     var month = time.slice(4, 7);
     var tarikhValue = time.slice(8, 10);
     var din = time.slice(0, 4);
     var year = time.slice(11, 15);
-    var ghanta = time.slice(16,18);
-    var minut = time.slice(19, 21);
-    var sec = time.slice(22, 24);
 
     day.innerHTML = din;
     maina.innerHTML = month;
     tarikh.innerHTML = tarikhValue;
     barsa.innerHTML = year
-    hour.innerHTML = ghanta;
-    minute.innerHTML = minut;
-    second.innerHTML = sec;
 }
+
+function myFunction(){
+  var timeDate = new Date();
+  var timeValue =   timeDate.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true });
+  samaya.innerHTML = timeValue;
+}
+
+
+setInterval(myFunction, 1000);
 
 setInterval(NepalDate, 1000);
 
 
-var time = new Date();
-console.log(
-  time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true })
-);
